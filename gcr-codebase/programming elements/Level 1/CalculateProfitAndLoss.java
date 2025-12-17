e0410629bc3d4802aa11150da4d877afe7ea72a5
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-17 14:15:21 +0530

    [Harshal] : Added problem 4 of level 1 on 17th Dec

---
+++ gcr-codebase/programming elements/Level 1/CalculateProfitAndLoss.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class CalculateProfitAndLoss{
+    public static void main(String[] args){
+        double cost_price = 129;
+		double selling_price = 191;
+		
+		double profit = selling_price-cost_price;
+		double profit_percentage = profit/cost_price*100;
+		
+		System.out.println("The Cost Price is INR "+ cost_price + " and Selling Price is INR "+selling_price+" The Profit is INR " + profit + " and the Profit Percentage is " + profit_percentage);
+    }
+}
