7a2525b0970299c812be5165c6a56a10f6546b98
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-26 14:30:02 +0530

    [Harshal] : Added Problem7ReverseInteger.java of leetcode

---
+++ leet-code-codebase/day 1/Problem7ReverseInteger.java
@@ -0,0 +1,28 @@
+class Problem7ReverseInteger{
+    public int reverse(int x){
+        int sum = 0;
+
+        while(x != 0){
+            int dig = x%10;
+
+            if(sum > Integer.MAX_VALUE/10 || sum < Integer.MIN_VALUE/10){
+                return 0;
+            }
+            sum = sum*10+dig;
+
+            x /= 10;
+        }
+        return sum;
+    }
+	
+	public static void main(String[] args){
+	    int x = 123;
+		
+		//Create object of a class
+		Problem7ReverseInteger lt = new Problem7ReverseInteger();
+		//Acccess method reverse through an object
+		int result = lt.reverse(x);
+		
+		System.out.println("Reversed number of "+x+" is: "+result);
+	}
+}
