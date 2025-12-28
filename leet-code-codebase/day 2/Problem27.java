4dc83c93ebf5a8b577f86d09e76eb6a61b376e21
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-29 00:37:32 +0530

    [Harshal] : Added Problem27.java of leetcode

---
+++ leet-code-codebase/day 2/Problem27.java
@@ -0,0 +1,20 @@
+class Problem27{
+    public static int removeElement(int[] nums, int val){
+        int cnt = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != val){
+                nums[cnt] = nums[i];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+	
+	public static void main(String[] args){
+	    int[] nums = {3,2,2,3};
+       	int val = 3;
+		
+		int result = removeElement(nums, val);
+		System.out.println("Elements is: "+ result);
+	}
+}
