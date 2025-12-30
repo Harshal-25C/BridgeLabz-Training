c18ef6720bd0f063cac7e8c054530d9ddd8188f7
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-31 00:30:51 +0530

    [Harshal] : Added Problem169.java of leetcode

---
+++ leet-code-codebase/day 4/Problem169.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Problem169{
+    public static int majorityElement(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        return nums[n/2];
+    }
+	
+	public static void main(String[] args){
+	    int[] nums = {3,2,3};
+		
+		int result = majorityElement(nums);
+		System.out.println("Majority Element is: " +result);
+	}
+}
