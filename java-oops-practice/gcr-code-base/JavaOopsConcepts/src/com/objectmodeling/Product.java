e35545c18a4bc79be19c0206a49139e99e1d9422
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-30 16:33:11 +0530

    [Harshal] : Product.java

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodeling/Product.java
@@ -0,0 +1,15 @@
+package com.objectmodeling;
+
+//Product class
+class Product {
+ String productName;
+ double price;
+
+ Product(String productName, double price){
+     this.productName = productName;
+     this.price = price;
+ }
+}
+
+
+
