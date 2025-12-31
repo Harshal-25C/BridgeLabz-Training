3ddcacce8f778c0874c70fe9548acd603d119bcd
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-31 22:04:06 +0530

    [Harshal] : Added ShippedOrder.java

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/ShippedOrder.java
@@ -0,0 +1,16 @@
+package com.inheritance.multilevelinheritance;
+
+public class ShippedOrder extends Order{
+	String trackingNumber;
+
+    ShippedOrder(String orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order Shipped";
+    }
+
+}
