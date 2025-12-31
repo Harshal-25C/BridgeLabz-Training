8174f6e387e17f3802dd2b9a5a50b02968fa6b31
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-31 16:34:20 +0530

    [Harshal] : Added BankAccount.java on 31th Dec

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/BankAccount.java
@@ -0,0 +1,12 @@
+package com.inheritance.hierarchicalinheritance;
+
+public class BankAccount{
+	String accountNumber;
+	double balance;
+	
+	public BankAccount(String accountNumber, double balance) {
+		// TODO Auto-generated constructor stub
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+}
