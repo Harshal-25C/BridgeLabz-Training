484ec1d126554c9294c2e412ed99f9460a00ce66
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-31 15:49:51 +0530

    [Harshal] : Added VehicleTransportDemo.java on 31 Dec

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/assistedproblems/VehicleTransportDemo.java
@@ -0,0 +1,17 @@
+package com.inheritance.assistedproblems;
+
+public class VehicleTransportDemo{
+    public static void main(String[] args){
+
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 10);
+        vehicles[2] = new Motorcycle(160, "Petrol");
+
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+            System.out.println();
+        }
+    }
+}
+
