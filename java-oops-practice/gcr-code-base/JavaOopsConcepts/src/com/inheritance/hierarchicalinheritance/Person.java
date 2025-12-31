23a6467d3e0d2b9155fa21fa83eac782b8220dc6
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-31 16:33:47 +0530

    [Harshal] : Added Person.java on 31th Dec

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/Person.java
@@ -0,0 +1,12 @@
+package com.inheritance.hierarchicalinheritance;
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
