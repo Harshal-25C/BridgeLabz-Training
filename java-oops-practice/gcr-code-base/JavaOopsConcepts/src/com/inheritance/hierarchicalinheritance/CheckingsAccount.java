b30aace38c08a1cc122b382c5bba80ce94ceae1d
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-31 16:36:59 +0530

    [Harshal] : Added CheckingsAccount.java on 31th Dec

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/CheckingsAccount.java
@@ -0,0 +1,16 @@
+package com.inheritance.hierarchicalinheritance;
+
+class CheckingAccount extends BankAccount {
+    double withdrawalLimit;
+
+    CheckingAccount(String accountNumber, double balance, double withdrawalLimit) {
+        super(accountNumber, balance);
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    void displayAccountType() {
+        System.out.println("Account Type: Checking Account");
+        System.out.println("Withdrawal Limit: " + withdrawalLimit);
+    }
+}
+
