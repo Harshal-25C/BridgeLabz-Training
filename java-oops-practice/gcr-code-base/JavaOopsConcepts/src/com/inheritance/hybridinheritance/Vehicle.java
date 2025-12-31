4294ec29b5c73ceff61aa2aa11c1e86fdc471fb7
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-31 22:30:40 +0530

    [Harshal] : Added Vehicle.java

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/hybridinheritance/Vehicle.java
@@ -0,0 +1,15 @@
+package com.inheritance.hybridinheritance;
+
+interface Refuelable {
+    void refuel();
+}
+
+class Vehicle {
+    int maxSpeed;
+    String model;
+
+    Vehicle(int maxSpeed, String model) {
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+    }
+}
