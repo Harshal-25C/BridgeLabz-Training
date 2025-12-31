f2dacfa14824778514b4d10b65d50f8f8e0fc9c3
Harshal Choudhary <harshalchoudhary340@gmail.com>
2025-12-31 15:52:21 +0530

    [Harshal] : Added Vehicle.java on 31th Dec

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/assistedproblems/Vehicle.java
@@ -0,0 +1,17 @@
+package com.inheritance.assistedproblems;
+
+class Vehicle{
+    int maxSpeed;
+    String fuelType;
+
+    Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed);
+        System.out.println("Fuel Type: " + fuelType);
+    }
+}
+
