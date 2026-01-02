58768720de89d0fef39123d8473bb94d1a7fc794
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-02 15:09:24 +0530

    [Harshal] : Added Course.java

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/day2/UniversityCourseEnrollmentSystem/Course.java
@@ -0,0 +1,15 @@
+package com.day2.UniversityCourseEnrollmentSystem;
+
+public class Course{
+
+	int courseId;
+    String courseName;
+    int credits;
+
+    Course(int id, String name, int credits) {
+        this.courseId = id;
+        this.courseName = name;
+        this.credits = credits;
+    }
+
+}
