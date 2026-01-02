8f34ca30b913976dac60a3869473bfa41546486b
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-02 16:54:13 +0530

    [Harshal-25C] : Added FriendNode.java on 2nd Jan

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/SinglyLinkedList/SocialMediaFriendConnections/FriendNode.java
@@ -0,0 +1,13 @@
+package com.linkedlist.SinglyLinkedList.SocialMediaFriendConnections;
+
+class FriendNode {
+ int friendId;
+ FriendNode next;
+
+ //Constructor to initialize friend ID
+ FriendNode(int friendId) {
+     this.friendId = friendId;
+     this.next = null;
+ }
+}
+
