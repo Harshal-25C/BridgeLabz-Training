f750cfe1a9e4adb7f392e1bafe9ad8fb94063a81
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-02 15:15:30 +0530

    [Harshal-25C]: added     Car.java

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/day2/VehicleRentalSystem/Car.java
@@ -0,0 +1,21 @@
+package com.day2.VehicleRentalSystem;
+
+public class Car extends Vehicle implements Insurable{
+    private String insurancePolicyNumber = "CAR12345";
+
+    Car(String number, double rate) {
+        super(number, "Car", rate);
+    }
+
+    public double calculateRentalCost(int days) {
+        return rentalRate * days;
+    }
+
+    public double calculateInsurance() {
+        return 500;
+    }
+
+    public String getInsuranceDetails() {
+        return "Car Insurance (Policy Hidden)";
+    }
+}
