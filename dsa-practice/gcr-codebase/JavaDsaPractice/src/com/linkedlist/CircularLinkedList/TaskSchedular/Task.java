6e55f9eac3d41dd676e10205c2adaaef6e2240db
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-02 17:13:48 +0530

    [Harshal-25C] : Added Task.java on 2nd Jan

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/CircularLinkedList/TaskSchedular/Task.java
@@ -0,0 +1,18 @@
+package com.linkedlist.CircularLinkedList.TaskSchedular;
+
+//Node class representing a Task
+class Task{
+ int taskId;
+ String taskName;
+ int priority;
+ String dueDate;
+ Task next;
+
+ Task(int taskId, String taskName, int priority, String dueDate){
+     this.taskId = taskId;
+     this.taskName = taskName;
+     this.priority = priority;
+     this.dueDate = dueDate;
+     this.next = null;
+ }
+}
