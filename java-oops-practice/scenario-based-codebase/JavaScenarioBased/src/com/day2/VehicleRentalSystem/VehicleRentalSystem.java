d8dca16310767ec1b606af6632b81cd9dfff5810
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-02 15:16:39 +0530

    [Harshal-25C]: added   VehicleRentalSystem.java

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/day2/VehicleRentalSystem/VehicleRentalSystem.java
@@ -0,0 +1,23 @@
+package com.day2.VehicleRentalSystem;
+import java.util.*;
+
+public class VehicleRentalSystem{
+    public static void main(String[] args){
+
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        vehicles.add(new Car("MH12A1234", 1500));
+        vehicles.add(new Bike("MH14B5678", 500));
+        vehicles.add(new Truck("MH10T9999", 3000));
+
+        for(Vehicle v : vehicles){
+            v.displayVehicle();
+            System.out.println("Rental Cost (3 days): ₹" + v.calculateRentalCost(3));
+
+            Insurable ins = (Insurable) v;
+            System.out.println("Insurance Cost: ₹" + ins.calculateInsurance());
+            System.out.println(ins.getInsuranceDetails());
+            System.out.println("----------------------------");
+        }
+    }
+}
