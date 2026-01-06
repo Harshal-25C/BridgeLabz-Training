d62423e4da9b5c283e6598ec50b8fedb84539a79
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-06 23:54:06 +0530

    [Harshal-25C] : Added User.java on 6th Jan

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/day5/EventEase/User.java
@@ -0,0 +1,16 @@
+package com.day5.EventEase;
+
+//User class
+class User {
+ private String name;
+ private String email;
+
+ User(String name, String email) {
+     this.name = name;
+     this.email = email;
+ }
+
+ public String getName() {
+     return name;
+ }
+}
