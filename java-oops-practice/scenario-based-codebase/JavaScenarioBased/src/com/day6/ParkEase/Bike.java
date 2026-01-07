bfa0116dcea4b78e354bf6ea4cb49dd3f264d026
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-07 15:49:58 +0530

    [Harshal] : Added Bike.java on 7th Jan

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/day6/ParkEase/Bike.java
@@ -0,0 +1,14 @@
+package com.day6.ParkEase;
+
+public class Bike extends Vehicle{
+    public Bike(String vehicleNumber) {
+        super(vehicleNumber, 30);
+    }
+
+    @Override
+    public double calculateCharges(int hours) {
+        int allowedHours = 6;
+        double penalty = hours > allowedHours?(hours-allowedHours)*10:0;
+        return (baseRate*hours)+penalty;
+    }
+}
