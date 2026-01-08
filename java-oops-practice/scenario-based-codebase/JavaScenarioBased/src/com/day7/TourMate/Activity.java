bdde68c5c577b24c885bac4060645d8879a00a92
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-08 14:17:24 +0530

    [Harshal-25C] : Added Activity.java on 8th Jan

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/day7/TourMate/Activity.java
@@ -0,0 +1,13 @@
+package com.day7.TourMate;
+
+public class Activity {
+    private double cost;
+
+    public Activity(double cost){
+        this.cost = cost;
+    }
+
+    double getCost(){
+        return cost;
+    }
+}
