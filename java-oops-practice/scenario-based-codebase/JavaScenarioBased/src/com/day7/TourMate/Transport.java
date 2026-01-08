82c3d1746ac3b2b32e6d693ac6a0d83bc52b20f1
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-08 14:16:20 +0530

    [Harshal-25C] : Added Transport.java on 8th Jan

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/day7/TourMate/Transport.java
@@ -0,0 +1,14 @@
+package com.day7.TourMate;
+
+public class Transport {
+    private double cost; //Encapsulated
+
+    public Transport(double cost){
+        this.cost=cost;
+    }
+
+    double getCost() {
+        return cost;
+    }
+}
+
