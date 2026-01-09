dafb24ef7410fc686c039d9ef61b865ac014abc0
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-09 16:30:27 +0530

    [Harshal] : Added User.java on 9th Jan

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/day8/OnlineLearningPortal/User.java
@@ -0,0 +1,13 @@
+package com.day8.OnlineLearningPortal;
+
+abstract class User{
+    protected String name;
+    protected String email;
+
+    public User(String name, String email){
+        this.name=name;
+        this.email=email;
+    }
+
+    public abstract void printDetails();
+}
