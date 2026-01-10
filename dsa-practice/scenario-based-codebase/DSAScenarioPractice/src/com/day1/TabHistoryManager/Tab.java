1c700bacd26cda3eaf8379b71b73160800564378
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-10 16:12:13 +0530

    [Harshal-25C] : Added Tab.java on 10th Jan

---
+++ dsa-practice/scenario-based-codebase/DSAScenarioPractice/src/com/day1/TabHistoryManager/Tab.java
@@ -0,0 +1,38 @@
+package com.day1.TabHistoryManager;
+
+class Tab {
+    private HistoryNode current;
+
+    public void visit(String url) {
+        HistoryNode newNode = new HistoryNode(url);
+
+        if(current!=null) {
+            current.next = newNode;
+            newNode.prev = current;
+        }
+        current = newNode;
+        System.out.println("Visited: "+url);
+    }
+
+    public void back() {
+        if(current!=null && current.prev!=null) {
+            current = current.prev;
+            System.out.println("Back to: " + current.url);
+        }else {
+            System.out.println("No previous page");
+        }
+    }
+
+    public void forward() {
+        if(current != null && current.next != null) {
+            current = current.next;
+            System.out.println("Forward to: "+current.url);
+        }else{
+            System.out.println("No next page");
+        }
+    }
+
+    public String getCurrentPage() {
+        return current != null ? current.url :"Empty Tab";
+    }
+}
