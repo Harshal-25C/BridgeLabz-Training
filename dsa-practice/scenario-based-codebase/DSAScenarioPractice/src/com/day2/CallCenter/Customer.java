0947ce8dca023ffacec4cba1ab12c0a33053f9d8
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-12 16:30:46 +0530

    [Harshal-25C] : Added Customer.java on 12th Jan

---
+++ dsa-practice/scenario-based-codebase/DSAScenarioPractice/src/com/day2/CallCenter/Customer.java
@@ -0,0 +1,13 @@
+package com.day2.CallCenter;
+
+class Customer{
+    int customerId;
+    String name;
+    boolean isVIP;
+
+    Customer(int customerId, String name, boolean isVIP) {
+        this.customerId = customerId;
+        this.name = name;
+        this.isVIP = isVIP;
+    }
+}
