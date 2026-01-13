6f8f089fb3659064c021ebf723ae26f9a2279d04
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-13 16:24:00 +0530

    [Harshal-25C] : Added Customer.java on 13th Jan

---
+++ dsa-practice/scenario-based-codebase/DSAScenarioPractice/src/com/day3/SupermarketBillingQueue/Customer.java
@@ -0,0 +1,13 @@
+package com.day3.SupermarketBillingQueue;
+
+import java.util.*;
+
+class Customer {
+    String name;
+    List<String> items;
+
+    Customer(String name, List<String> items) {
+        this.name = name;
+        this.items = items;
+    }
+}
