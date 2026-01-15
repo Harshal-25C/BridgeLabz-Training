d7a5d53c7e77a4b43c7d284da51e34fa921ef439
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-15 15:01:03 +0530

    [Harshal-25C] : Added Book.java on 15th Jan

---
+++ dsa-practice/scenario-based-codebase/DSAScenarioPractice/src/com/day4/RealTimeBookArrangement/Book.java
@@ -0,0 +1,17 @@
+package com.day4.RealTimeBookArrangement;
+
+public class Book {
+    private String title;
+
+    public Book(String title) {
+        this.title=title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String toString() {
+        return title;
+    }
+}
