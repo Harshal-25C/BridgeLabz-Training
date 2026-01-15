b1cd394932e2e4cb94a9e51ad878268e441595c0
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-15 16:00:24 +0530

    [Harshal-25C] : Added Resume.java

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/generics/AIDrivenResumeScreeningSystem/Resume.java
@@ -0,0 +1,20 @@
+package com.generics.AIDrivenResumeScreeningSystem;
+
+public class Resume<T extends JobRole> {
+
+    private String candidateName;
+    private T jobRole;
+
+    public Resume(String candidateName, T jobRole) {
+        this.candidateName=candidateName;
+        this.jobRole=jobRole;
+    }
+
+    public T getJobRole() {
+        return jobRole;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+}
