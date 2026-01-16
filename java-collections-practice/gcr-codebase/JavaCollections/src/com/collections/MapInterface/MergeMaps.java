d1b49e065d4823667f7f7183f315e532031aed6e
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-16 23:50:20 +0530

    [Harshal-25C] : Added MergeMaps.java

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/collections/MapInterface/MergeMaps.java
@@ -0,0 +1,25 @@
+package com.collections.MapInterface;
+import java.util.*;
+
+public class MergeMaps {
+
+    public static Map<String, Integer> merge(
+            Map<String, Integer> map1,
+            Map<String, Integer> map2) {
+
+        Map<String, Integer> result = new HashMap<>(map1);
+
+        for(Map.Entry<String, Integer> entry:map2.entrySet()) {
+            result.merge(entry.getKey(), entry.getValue(), Integer::sum);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> map1 = Map.of("A", 1, "B", 2);
+        Map<String, Integer> map2 = Map.of("B", 3, "C", 4);
+
+        System.out.println(merge(map1, map2));
+    }
+}
