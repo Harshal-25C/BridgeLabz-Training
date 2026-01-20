fd24725e6aaa4eb64bf2c05627edc83f43a3d991
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-20 17:03:16 +0530

    [Harshal-25C] : Added ExceptionPropagationExample.java

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/exceptions/ExceptionPropagationExample.java
@@ -0,0 +1,22 @@
+package com.exceptions;
+
+public class ExceptionPropagationExample {
+
+    static void method1() {
+        int result = 10/0; //ArithmeticException
+    }
+
+    static void method2() {
+        method1();
+    }
+
+    public static void main(String[] args) {
+
+        try{
+            method2();
+
+        }catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
