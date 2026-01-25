e77bc6c0ad436f071b9dbfeb8deeced89a9094fd
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-25 21:10:33 +0530

    [Harshal-25C] : Added MethodExecutionTiming.java

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflection/AdvancedLevelPrograms/MethodExecutionTiming.java
@@ -0,0 +1,25 @@
+package com.reflection.AdvancedLevelPrograms;
+
+import java.lang.reflect.*;
+
+public class MethodExecutionTiming {
+
+    static class Task {
+        public void longTask() throws Exception {
+            Thread.sleep(1000);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Task task = new Task();
+
+        Method method = Task.class.getMethod("longTask");
+
+        long start = System.nanoTime();
+        method.invoke(task);
+        long end = System.nanoTime();
+
+        System.out.println("Execution Time: " + (end - start)/1_000_000 + " ms");
+    }
+}
