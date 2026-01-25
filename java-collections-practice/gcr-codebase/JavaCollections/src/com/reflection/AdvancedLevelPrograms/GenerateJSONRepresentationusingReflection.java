e283264de7f439289d4466b2557389633591956d
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-01-25 21:08:56 +0530

    [Harshal-25C] : Added GenerateJSONRepresentationusingReflection.java

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflection/AdvancedLevelPrograms/GenerateJSONRepresentationusingReflection.java
@@ -0,0 +1,39 @@
+package com.reflection.AdvancedLevelPrograms;
+
+import java.lang.reflect.*;
+
+public class GenerateJSONRepresentationusingReflection {
+
+	    public static String toJson(Object obj) throws Exception {
+
+	        StringBuilder json = new StringBuilder("{");
+	        Class<?> cls = obj.getClass();
+
+	        for(Field f : cls.getDeclaredFields()) {
+	            f.setAccessible(true);
+	            json.append("\"").append(f.getName()).append("\":\"")
+	                .append(f.get(obj))
+	                .append("\",");
+	        }
+
+	        json.deleteCharAt(json.length() - 1);
+	        json.append("}");
+
+	        return json.toString();
+	    }
+
+	    static class Student {
+	        String name;
+	        int age;
+
+	        Student(String n, int a) {
+	            name = n;
+	            age = a;
+	        }
+	    }
+
+	    public static void main(String[] args) throws Exception {
+	        Student s = new Student("Harshal", 20);
+	        System.out.println(toJson(s));
+	    }
+	}
