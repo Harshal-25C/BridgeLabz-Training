2ec63cda3d2da30bbaa71c80aa53cf33b197ff7b
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-02-04 11:50:27 +0530

    [Harshal-25C] : Add Future-Logistics program

---
+++ dsa-practice/scenario-based-codebase/DSAScenarioPractice/src/com/AssessmentLevelPrograms/FutureLogistics/Utility.java
@@ -0,0 +1,46 @@
+package com.AssessmentLevelPrograms.FutureLogistics;
+
+public class Utility {
+
+    public boolean validateTransportId(String transportId) {
+        return transportId.matches("RTS\\d{3}[A-Z]");
+    }
+
+    public GoodsTransport parseDetails(String input) {
+
+        String[] data = input.split(":");
+
+        String transportId = data[0];
+        String transportDate = data[1];
+        int rating = Integer.parseInt(data[2]);
+        String transportType = data[3];
+
+        if (transportType.equalsIgnoreCase("BrickTransport")) {
+            return new BrickTransport(
+                    transportId,
+                    transportDate,
+                    rating,
+                    Float.parseFloat(data[4]),
+                    Integer.parseInt(data[5]),
+                    Float.parseFloat(data[6])
+            );
+        } else {
+            return new TimberTransport(
+                    transportId,
+                    transportDate,
+                    rating,
+                    Float.parseFloat(data[4]),
+                    Float.parseFloat(data[5]),
+                    data[6],
+                    Float.parseFloat(data[7])
+            );
+        }
+    }
+
+    public String findObjectType(GoodsTransport goodsTransport) {
+        if (goodsTransport instanceof TimberTransport)
+            return "TimberTransport";
+        else
+            return "BrickTransport";
+    }
+}
