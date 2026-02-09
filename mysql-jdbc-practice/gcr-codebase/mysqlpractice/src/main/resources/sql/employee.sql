6a7a9fbd9e591691c9a234a20a1d4f14466287e9
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-02-09 13:23:26 +0530

    [Harshal-25C] : Update employee.sql file

--- mysql-jdbc-practice/gcr-codebase/mysqlpractice/src/main/resources/sql/employee.sql
+++ mysql-jdbc-practice/gcr-codebase/mysqlpractice/src/main/resources/sql/employee.sql
@@ -1,6 +1,38 @@
 CREATE TABLE employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
+    emp_id INT PRIMARY KEY AUTO_INCREMENT,
+    emp_name VARCHAR(100),
     salary DOUBLE(),
     department VARCHAR(50)
 );
+
+mysql> SELECT * FROM employee;
++--------+-------------------+-----------+------------+
+| emp_id | emp_name          | salary    | department |
++--------+-------------------+-----------+------------+
+|      1 | Harshal Choudhary | 130000.00 | NULL       |
+|      2 | Ayush Mishra      | 120000.00 | NULL       |
+|      3 | Devarshi Mishra   | 120000.00 | NULL       |
+|      4 | John Doe          |  20000.00 | NULL       |
+|      5 | Hermione Granger  | 150000.00 | NULL       |
++--------+-------------------+-----------+------------+
+5 rows in set (0.08 sec)
+
+mysql> INSERT INTO employee(emp_id, emp_name, salary, department) VALUES
+    -> (101, 'Abhishek', 55000, 'AIML'),
+    -> (102, 'Rajeev', 40000, 'EC');
+Query OK, 2 rows affected (0.07 sec)
+Records: 2  Duplicates: 0  Warnings: 0
+
+mysql> SELECT * FROM employee;
++--------+-------------------+-----------+------------+
+| emp_id | emp_name          | salary    | department |
++--------+-------------------+-----------+------------+
+|      1 | Harshal Choudhary | 130000.00 | NULL       |
+|      2 | Ayush Mishra      | 120000.00 | NULL       |
+|      3 | Devarshi Mishra   | 120000.00 | NULL       |
+|      4 | John Doe          |  20000.00 | NULL       |
+|      5 | Hermione Granger  | 150000.00 | NULL       |
+|    101 | Abhishek          |  55000.00 | AIML       |
+|    102 | Rajeev            |  40000.00 | EC         |
++--------+-------------------+-----------+------------+
+7 rows in set (0.01 sec)
