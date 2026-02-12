d15c419e19ca0551ef7fdb5850368826ca59aefa
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-02-13 00:46:28 +0530

    [Harshal-25C] : Add RobotHazardAuditor.java

--- dsa-practice/scenario-based-codebase/DSAScenarioPractice/src/com/AssessmentLevelPrograms/FactoryRobotHazardAnalyzer/RobotHazardAuditor.java
+++ dsa-practice/scenario-based-codebase/DSAScenarioPractice/src/com/AssessmentLevelPrograms/FactoryRobotHazardAnalyzer/RobotHazardAuditor.java
@@ -1,16 +1,47 @@
 package com.AssessmentLevelPrograms.FactoryRobotHazardAnalyzer;
+import java.util.*;
 
 public class RobotHazardAuditor {
-	public double CalculateHazardRisk(double armPrecision, int workerDensity, String machineryState) {
-		
-		String pk= "jbb";
-		
-		pk = "39r3-";
-		
-		pk.toLowerCase();
-		
-		double hk=0;
-		return hk;
-		
-	}
+
+    public double CalculateHazardRisk(double armPrecision, int workerDensity, 
+    		String machineryState) throws RobotSafetyException {
+
+        // Validate armPrecision
+        if(armPrecision < 0.0 || armPrecision > 1.0) {
+            throw new RobotSafetyException(
+                    "Error: Arm precision must be 0.0-1.0"
+            );
+        }
+
+        // Validate workerDensity
+        if(workerDensity < 1 || workerDensity > 20) {
+            throw new RobotSafetyException(
+                    "Error: Worker density must be 1-20"
+            );
+        }
+
+        // Validate machineryState (case-sensitive)
+        double machineRiskFactor;
+
+        switch (machineryState) {
+            case "Worn":
+                machineRiskFactor = 1.3;
+                break;
+            case "Faulty":
+                machineRiskFactor = 2.0;
+                break;
+            case "Critical":
+                machineRiskFactor = 3.0;
+                break;
+            default:
+                throw new RobotSafetyException(
+                        "Error: Unsupported machinery state"
+                );
+        }
+
+        // Hazard Risk Formula
+        double hazardRisk = ((1.0 - armPrecision)*15.0) + (workerDensity*machineRiskFactor);
+
+        return hazardRisk;
+    }
 }
