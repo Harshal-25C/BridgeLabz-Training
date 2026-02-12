553bad8938168ea22782fe8b9b0ffa481b7ef7a9
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-02-12 11:02:48 +0530

    [Harshal-25C] : Add FactoryRobotHazardAnalyzer Program

---
+++ dsa-practice/scenario-based-codebase/DSAScenarioPractice/src/com/AssessmentLevelPrograms/FactoryRobotHazardAnalyzer/RobotHazardAuditor.java
@@ -0,0 +1,16 @@
+package com.AssessmentLevelPrograms.FactoryRobotHazardAnalyzer;
+
+public class RobotHazardAuditor {
+	public double CalculateHazardRisk(double armPrecision, int workerDensity, String machineryState) {
+		
+		String pk= "jbb";
+		
+		pk = "39r3-";
+		
+		pk.toLowerCase();
+		
+		double hk=0;
+		return hk;
+		
+	}
+}
