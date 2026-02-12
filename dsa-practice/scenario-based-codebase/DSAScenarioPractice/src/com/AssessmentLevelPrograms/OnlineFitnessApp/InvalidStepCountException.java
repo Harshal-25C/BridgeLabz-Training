f36a9a4a081ab5aeb09457cec70eb17b68b8aa22
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-02-12 11:03:34 +0530

    [Harshal-25C] : Add OnlineFitnessApp Program

---
+++ dsa-practice/scenario-based-codebase/DSAScenarioPractice/src/com/AssessmentLevelPrograms/OnlineFitnessApp/InvalidStepCountException.java
@@ -0,0 +1,8 @@
+package com.AssessmentLevelPrograms.OnlineFitnessApp;
+
+public class InvalidStepCountException extends Exception {
+
+    public InvalidStepCountException(String message) {
+        super(message);
+    }
+}
